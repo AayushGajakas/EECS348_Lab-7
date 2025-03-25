49d8d2cea989223eb19a3d0217021c22e148fd7c
AayushGajakas <135060755+AayushGajakas@users.noreply.github.com>
2025-03-25 11:01:52 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,19 @@
+#include <stdio.h>
 #include "football.h"
 
-int main(int argc, char *argv[]) {
+int main() {
+    int score;
+    printf("Enter NFL score: ");
+    scanf("%d", &score);
+
+    if (score < 2) {
+        printf("Invalid score.\n");
+        return 1;
+    }
+
+    print_combinations(score);
+    int total = count_combinations(score);
+    printf("\nNumber of possible scoring combinations: %d\n", total);
+
     return 0;
 }
